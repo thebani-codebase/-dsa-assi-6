6b6bb04cc1a30d9bfea7845d0d92e914c3b23af4
thebani-codebase <bbani_be24@thapar.edu>
2025-11-26 08:37:01 +0530

    Add sorting algorithms in ques1.cpp
    Implemented selection sort, insertion sort, bubble sort, merge sort, and quick sort algorithms.

---
+++ ques1.cpp
@@ -0,0 +1,162 @@
+/*
+Name: Bani
+Roll No: 102403190
+Question 1: Write a program to implement following sorting techniques:
+ Selection Sort
+ Insertion Sort
+ Bubble Sort
+Merge Sort
+ Quick Sort
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx])
+                minIdx = j;
+        }
+        swap(arr[i], arr[minIdx]);
+    }
+}
+
+
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+}
+
+
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    
+    vector<int> L(n1), R(n2);
+    
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+    
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(vector<int>& arr) {
+    for (int x : arr)
+        cout << x << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
+    
+    vector<int> temp = arr;
+    selectionSort(temp);
+    cout << "Selection Sort: ";
+    printArray(temp);
+    
+    temp = arr;
+    insertionSort(temp);
+    cout << "Insertion Sort: ";
+    printArray(temp);
+    
+    temp = arr;
+    bubbleSort(temp);
+    cout << "Bubble Sort: ";
+    printArray(temp);
+    
+    temp = arr;
+    mergeSort(temp, 0, temp.size() - 1);
+    cout << "Merge Sort: ";
+    printArray(temp);
+    
+    temp = arr;
+    quickSort(temp, 0, temp.size() - 1);
+    cout << "Quick Sort: ";
+    printArray(temp);
+    
+    return 0;
+}
